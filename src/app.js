5ad3992d5574e624643842fdb7e7ac512a142d49
zrajesh <rajesh.bhattarai@beatoapp.com>
2024-07-17 15:30:44 +0530

    DB connection and code refactor

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import logger from "./utils/logger.js";
+
+const app = express();
+
+const morganFormat = ":method :url :status :response-time ms";
+
+// Middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import morgan from "morgan";
-import logger from "./utils/logger.js";
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "././src/.env" });
 
-const app = express();
 const port = process.env.PORT || 5000;
 
-const morganFormat = ":method :url :status :response-time ms";
-
-// Middlewares
-app.use(express.json());
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
   })
-);
+  .catch((error) => {
+    console.log("DB CONNECTION ERROR: ", error);
+  });
